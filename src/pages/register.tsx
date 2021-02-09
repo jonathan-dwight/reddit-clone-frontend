95fab9bdd0fc156403416289b2d66907ff07a2d6
jonathan-dwight <jd.buendia0711@gmail.com>
2021-02-08 21:40:28 -0800

    add graphql code generator

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Formik, Form } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { Box, Button, propNames } from "@chakra-ui/react";
-import { useMutation } from "urql";
+import { Box, Button } from "@chakra-ui/react";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
@@ -12,34 +12,20 @@ interface registerProps {}
 
 // next js will always make it a route in next JS in pages
 
-const REGISTER_MUT = `
-    mutation Register($username: String!, $password: String!) {
-        register(options: {username: $username, password: $password}) {
-            errors {
-                field
-                message
-            }
-        
-            user {
-            id
-            username
-            }
-        }
-    }
-`;
 
 export const Register: React.FC<registerProps> = ({}) => {
-    const [, register] = useMutation(REGISTER_MUT);
+    const [, register] = useRegisterMutation();
 
     return (
         <Wrapper variant="small">
             <Formik
                 initialValues={{ username: "", password: "" }}
-                onSubmit={(values) => {
-                    return register(values)
+                onSubmit={async (values) => {
+                    const response = await register(values);
+                    response.data.register.user.id
                 }}
             >
-                {({ values, handleChange }) => (
+                {({ isSubmitting }) => (
                     <Form>
                         <InputField
                             name="username"
@@ -54,7 +40,12 @@ export const Register: React.FC<registerProps> = ({}) => {
                                 type="password"
                             />
                         </Box>
-                        <Button mt={4} type="submit" color="teal">
+                        <Button
+                            mt={4}
+                            type="submit"
+                            isLoading={isSubmitting}
+                            color="teal"
+                        >
                             Register
                         </Button>
                     </Form>
