d102be98637b01c8fbf328527fba336be9b252e9
jonathan-dwight <jd.buendia0711@gmail.com>
2021-02-08 21:20:31 -0800

    add urql for graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,31 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import { ThemeProvider } from '@emotion/react';
+import { createClient, Provider } from "urql";
+
+const client = createClient({ 
+    url: "http://localhost:5000/graphql",
+    // sending cookie
+    fetchOptions: {
+        credentials: "include",
+    }
+
+})
 
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ThemeProvider theme={theme}>
-            <ColorModeProvider
-                options={{
-                    useSystemColorMode: true,
-                }}
-            >
-                <Component {...pageProps} />
-            </ColorModeProvider>
-        </ThemeProvider>
+        <Provider value={client}>
+            <ThemeProvider theme={theme}>
+                <ColorModeProvider
+                    options={{
+                        useSystemColorMode: true,
+                    }}
+                >
+                    <Component {...pageProps} />
+                </ColorModeProvider>
+            </ThemeProvider>
+        </Provider>
     );
 }
 
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,21 +3,40 @@ import { Formik, Form } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box, Button, propNames } from "@chakra-ui/react";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
 // have to learn how to do the loading on the button?
-//isLoading={isSubmitting} 
+//isLoading={isSubmitting}
 
 // next js will always make it a route in next JS in pages
 
+const REGISTER_MUT = `
+    mutation Register($username: String!, $password: String!) {
+        register(options: {username: $username, password: $password}) {
+            errors {
+                field
+                message
+            }
+        
+            user {
+            id
+            username
+            }
+        }
+    }
+`;
+
 export const Register: React.FC<registerProps> = ({}) => {
+    const [, register] = useMutation(REGISTER_MUT);
+
     return (
         <Wrapper variant="small">
             <Formik
                 initialValues={{ username: "", password: "" }}
-                onSubmit={(values, actions) => {
-                    console.log(values);
+                onSubmit={(values) => {
+                    return register(values)
                 }}
             >
                 {({ values, handleChange }) => (
